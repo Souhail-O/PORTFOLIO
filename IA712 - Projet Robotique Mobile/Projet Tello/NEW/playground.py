768b2fda8da76f67d17d5a7f39ad4816bb504df3
Souhail OUMAMA <souhail.oumama@telecom-paris.fr>
2022-08-10 18:33:36 +0200

    Update

--- IA712 - Projet Robotique Mobile/Projet Tello/NEW/playground.py
+++
@@ -1,9 +0,0 @@
-from  djitellopy import tello 
-import cv2
-import numpy as np
-from time import sleep
-
-
-me = tello.Tello()
-me.connect()
-print("---- Battery : {}% ----".format(me.get_battery()))
